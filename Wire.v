705984d9bc2212a8b4efdf320703bd134e173c1f
Atharva Agiwal <130425572+atharvaagiwal2@users.noreply.github.com>
2023-06-10 13:50:37 +0530

    Create Wire.v

---
+++ Wire.v
@@ -0,0 +1,4 @@
+module top_module( input in, output out );
+assign out = in; 
+  //wire is directional, this means information flows in only one direction, from (usually one) source to the sinks
+endmodule
