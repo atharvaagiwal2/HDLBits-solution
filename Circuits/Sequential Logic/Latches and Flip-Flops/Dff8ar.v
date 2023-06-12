d3e8e22fbfd80bba4d620ac6024ad05810b3dec7
Atharva Agiwal <130425572+atharvaagiwal2@users.noreply.github.com>
2023-06-12 23:46:16 +0530

    Create Dff8ar.v

---
+++ Circuits/Sequential Logic/Latches and Flip-Flops/Dff8ar.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always @( posedge clk or posedge areset)
+        begin
+            q <= areset ? '0 : d;
+        end
+
+endmodule
