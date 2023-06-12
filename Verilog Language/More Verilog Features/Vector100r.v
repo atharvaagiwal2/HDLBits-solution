8233bd06472338420b964d4e913a7bee8ccd595a
Atharva Agiwal <130425572+atharvaagiwal2@users.noreply.github.com>
2023-06-12 09:19:24 +0530

    Create Vector100r.v

---
+++ Verilog Language/More Verilog Features/Vector100r.v
@@ -0,0 +1,12 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    always @(*)
+        begin
+            for(int i = 0; i < 100; i = i + 1)
+                begin
+                    out[i] = in [99-i];
+                end
+        end
+endmodule
