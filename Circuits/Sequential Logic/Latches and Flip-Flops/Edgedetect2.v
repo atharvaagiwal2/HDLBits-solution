20dba817ea0eaa87eab996874bb453c6847b314a
Atharva Agiwal <130425572+atharvaagiwal2@users.noreply.github.com>
2023-06-13 12:31:11 +0530

    Create Edgedetect2.v

---
+++ Circuits/Sequential Logic/Latches and Flip-Flops/Edgedetect2.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    reg [7:0] temp;
+    always @(posedge clk)
+        begin
+            temp<=in;
+            anyedge <= temp ^ in;
+        end
+
+endmodule
