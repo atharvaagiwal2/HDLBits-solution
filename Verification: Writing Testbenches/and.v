60bdf82701570eb1d4bc9d4d5eee64f65d51cae4
Atharva Agiwal <130425572+atharvaagiwal2@users.noreply.github.com>
2023-07-04 08:50:04 +0530

    Create and.v

---
+++ Verification: Writing Testbenches/and.v
@@ -0,0 +1,14 @@
+module top_module();
+    reg [1:0] in;
+    wire out;
+    
+    andgate dut(in,out);
+    
+    initial 
+        begin
+            in = 2'b00;
+            #10 in = 2'b01;
+            #10 in = 2'b10;
+            #10 in = 2'b11;
+        end    
+endmodule
